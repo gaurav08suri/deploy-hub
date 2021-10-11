b99ae297b66d73cfa77828142076f2fadf38a3cf
Gaurav Suri <gaurav08suri@gmail.com>
2021-10-11 19:33:07 +0530

    Inital commit for PoC to clone repo and export Issues and PRs

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # deploy-hub
+
+## Installation
+* `python -m venv venv`
+* `source venv\bin\activate`
+* `pip install -r requirements.txt`
---
+++ tasks/github/backup.py
@@ -0,0 +1,43 @@
+import os
+from datetime import datetime
+from github import Github
+from pyinfra.operations import git, server, pip, files
+# from gh2md import gh2md
+
+token = os.environ['GITHUB_ACCESS_TOKEN']
+client = Github(token)
+#  = 'xxxxxxxxx'
+date_prefix = datetime.today().strftime('%Y-%m-%d')
+
+# Fetch all repos of the organization
+for repo in client.get_organization('dadabhagwan').get_repos():
+    print(repo.name)
+
+    files.directory(
+        name='Ensure the /tmp/dir_that_we_want_removed is removed',
+        path=f"./{date_prefix}/issues/{repo.name}",
+        present=True,
+    )
+
+    if repo.name != 'mba-sys':
+        continue
+    
+    pip.venv(
+        name='Create a virtual env',
+        path='./venv'
+    )
+
+    pip.packages(
+        packages=['gh2md'],
+        virtualenv='./venv'
+    )
+
+    # server.shell(commands=[f"gh2md {repo.full_name} --multiple-files ./{date_prefix}/issues/{repo.name}"])
+
+    # Clone the repo
+    git.repo(
+        src=repo.url,
+        dest=f"./{date_prefix}/{repo.full_name}",
+        branch=None
+    )
+    # gh2md sarabander/sicp sicp-issues --multiple-files --no-closed-prs
